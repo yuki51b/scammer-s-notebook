7db85030441705392e9e8c58a11ce6524513755e
yukimura51b <yukimura.3720@gmail.com>
2024-06-08 16:07:12 +0900

    cssとdaizyUIの反映確認

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Myapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
     # Don't generate system test files.
     config.generators.system_tests = nil
     config.generators do |g|
