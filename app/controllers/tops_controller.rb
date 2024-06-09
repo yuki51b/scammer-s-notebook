a3ab4eae7e160baf7393baa326d37413f7d9d7fe
yukimura51b <yukimura.3720@gmail.com>
2024-06-09 14:54:22 +0900

    gem導入エラーなし

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,3 +1,3 @@
 class TopsController < ApplicationController
-    def index; end
+  def index; end
 end
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -14,7 +14,7 @@ module Myapp
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -23,7 +23,7 @@ module Myapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    
+
     # Don't generate system test files.
     config.generators.system_tests = nil
     config.generators do |g|
