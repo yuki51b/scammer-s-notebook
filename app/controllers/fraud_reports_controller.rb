57b11722ce9602f7498596507705406e0bb5c5ec
yukimura51b <yukimura.3720@gmail.com>
2024-06-17 02:40:43 +0900

    とりあえず、scamテーブルから情報取り出す診断結果の処理のみ完了。コントローラーをまとめること。対策ポイントの処理もする

--- app/controllers/fraud_reports_controller.rb
+++ app/controllers/fraud_reports_controller.rb
@@ -30,13 +30,33 @@ class FraudReportsController < ApplicationController
             対話型の返答は省き、詐欺名(悪質商法も含む)のみを返答してください。
             PROMPT
 
+            response = ChatgptService.call(prompt) # ここで設定したAPIのメソッドを使っている
+            Rails.logger.info "ChatGPT API response: #{response}"
+            @fraud_report.respond = response # respondカラムという保存先を指定
+
+
+            # まず、詐欺診断処理を行って詐欺情報を確定させる
+            scam = handle_scam_diagnosis(response)
+
+            if scam.nil?
+              redirect_to root_path, alert: '詐欺情報の生成に失敗しました。'
+              return
+            end
+
+            # ここで `FraudReport` に `scam_id` を設定します
+            @fraud_report.scam = scam
+
+
         begin # エラーが出そうな部分を指定。
-            responce = ChatgptService.call(prompt) # ここで設定したAPIのメソッドを使っている
-            logger.info "OpenAI API response: #{response}" # レスポンスをログに出力
-                @fraud_report.respond = responce # respondカラムという保存先を指定
+            # response = ChatgptService.call(prompt) # ここで設定したAPIのメソッドを使っている
+            # Rails.logger.info "ChatGPT API response: #{response}"
+            #     @fraud_report.respond = response # respondカラムという保存先を指定
+
                 if @fraud_report.save
+                    # handle_scam_diagnosis(response) # 診断結果をscamテーブルに情報があるかチェック
                     redirect_to fraud_report_path(@fraud_report), notice: '診断名を受け取りました'
                 else
+                    logger.error "Failed to save fraud_report: #{@fraud_report.errors.full_messages.join(', ')}"
                     redirect_to root_path, alert: '保存に失敗しました。'
                 end
             rescue Net::ReadTimeout, StandardError => e # beginの箇所で失敗した場合のエラーをキャッチして処理
@@ -45,13 +65,39 @@ class FraudReportsController < ApplicationController
         end
     end
 
-    def show 
+    def show
         @fraud_report = FraudReport.find(params[:id])
+        @scam = Scam.find_by(name: @fraud_report.respond)
     end
 
   private
 
     def fraud_report_params
         params.require(:fraud_report).permit(:contact_method, :contact_content, :information, :urgent_action, :payment_method, :company_info, :additional_details)
+    end
+
+    def handle_scam_diagnosis(response_name)
+        scam = Scam.find_by(name: response_name)
+        return scam if scam
+
+        #ここに詐欺名がなかった場合の処理を記載
+        scam_content_prompt =
+                        <<~PROMPT
+                            詐欺名: #{response_name}
+                            上記の詐欺についての詳細を一言で教えてください。
+                            対話型の返答は省き、日本語で詐欺の詳細の1行だけを返答してください。
+                        PROMPT
+
+        scam_content = ChatgptService.call(scam_content_prompt)
+        Rails.logger.info "ChatGPT API response: #{scam_content}"
+        scam = Scam.new(name: response_name, content: scam_content)
+
+        if scam.save
+            Rails.logger.info "ChatGPT API response: #{scam_content}"
+            return scam
+        else
+            logger.error "Failed to save scam: #{scam.errors.full_messages.join(', ')}"
+            return nil
+        end
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_12_124448) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_15_144821) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_12_124448) do
     t.string "respond"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "scam_id", null: false
+    t.index ["scam_id"], name: "index_fraud_reports_on_scam_id"
   end
 
+  create_table "scams", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "fraud_reports", "scams"
 end
