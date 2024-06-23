68ebfda4151fab6f800e47cab1fe776f43499913
yukimura51b <yukimura.3720@gmail.com>
2024-06-23 22:47:21 +0900

    新規投稿機能の作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,27 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.includes(:user)
+    @posts = Post.includes(:user, :scam)
+  end
+
+  def new
+    @post = Post.new
+    @scams = Scam.all
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+      if @post.save
+        redirect_to posts_path, notice: '投稿できました'
+      else
+        flash.now[:alert] = "投稿に失敗しました"
+        @scams = Scam.all
+        render :new
+      end
+  end
+
+private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :scam_id)
   end
 end
--- app/models/scam.rb
+++ app/models/scam.rb
@@ -1,3 +1,4 @@
 class Scam < ApplicationRecord
     has_many :fraud_report
+    has_many :posts
 end
