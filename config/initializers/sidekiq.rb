4b72cac6b67e2d8f82ff2f5f42c45a017ca605cb
yukimura51b <yukimura.3720@gmail.com>
2024-07-25 13:10:28 +0900

    .envファイルの使用

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,9 @@
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: Rails.env.production? ? Rails.application.credentials.redis[:production] : Rails.application.credentials.redis[:development] }
+  config.redis = { url: Rails.env.production? ? ENV['REDIS_URL_PRODUCTION'] : Rails.application.credentials.redis[:development] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: Rails.env.production? ? Rails.application.credentials.redis[:production] : Rails.application.credentials.redis[:development] }
+  config.redis = { url: Rails.env.production? ? ENV['REDIS_URL_PRODUCTION'] : Rails.application.credentials.redis[:development] }
 end
+
