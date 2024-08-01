baa607905a9a7da5043f609b087ed992062d5f51
yukimura51b <yukimura.3720@gmail.com>
2024-08-01 16:29:44 +0900

    エラーデバック

--- app/controllers/concerns/ogp_creator.rb
+++ app/controllers/concerns/ogp_creator.rb
@@ -1,5 +1,4 @@
 class OgpCreator
-
   require 'mini_magick'
   BASE_IMAGE_PATH = './app/assets/images/X_post.jpg'
   GRAVITY = 'center'
@@ -12,12 +11,18 @@ class OgpCreator
   def self.build(text)
     text = prepare_text(text)
     image = MiniMagick::Image.open(BASE_IMAGE_PATH)
-    image.combine_options do |config|
-      config.font FONT
-      config.fill '#0f172a'
-      config.gravity GRAVITY
-      config.pointsize FONT_SIZE
-      config.draw "text #{TEXT_POSITION} '#{text}'"
+    begin #デバック
+      image.combine_options do |config|
+        config.font FONT
+        config.fill '#0f172a'
+        config.gravity GRAVITY
+        config.pointsize FONT_SIZE
+        config.draw "text #{TEXT_POSITION} '#{text}'"
+      end
+      image
+    rescue MiniMagick::Error => e
+      Rails.logger.error "MiniMagick Error: #{e.message}"
+      raise
     end
   end
 
