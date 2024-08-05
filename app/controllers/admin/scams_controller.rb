fa18b36cfaee1ca647bf0a9f4d833fd729b636cc
yukimura51b <yukimura.3720@gmail.com>
2024-08-05 16:11:41 +0900

    詐欺情報作成機能と詳細ページまで作成

--- app/controllers/admin/scams_controller.rb
+++ app/controllers/admin/scams_controller.rb
@@ -1,6 +1,33 @@
 class Admin::ScamsController < Admin::BaseController
 
   def index
-    @scams = Scam.all
+    @scams = Scam.all.order("name")
   end
+
+  def new
+    @scam = Scam.new
+  end
+
+  def create
+    @scam = Scam.new(scam_params)
+
+    if @scam.save
+      redirect_to admin_scams_path, notice: "詐欺情報を作成しました"
+    else
+      render :new, error: "作成に失敗しました"
+    end
+  end
+
+  def show
+    @scam = Scam.find(params[:id])
+    @scam_strategy = @scam.scam_strategy.split("\n")
+  end
+
+private
+
+def scam_params
+  params.require(:scam).permit(:name, :content, :point_1, :point_2, :point_3, :scam_strategy)
+end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root "scams#index"
-    resource :scams
+    resources :scams
     get 'login' => 'user_sessions#new', :as => :login
     post 'login' => "user_sessions#create"
     delete 'logout' => 'user_sessions#destroy', :as => :logout
