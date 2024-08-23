83d4ec5cfbb89bfc50592cbb4b8e492311fab373
yukimura51b <yukimura.3720@gmail.com>
2024-08-23 15:00:46 +0900

    userモデルのRspec

---
+++ spec/factories/user.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    name { "マドフくん" }
+    sequence(:email) { |n| "user#{n}@example.com" }
+    password { "password" }
+    password_confirmation { "password" }
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    # 各テストコードが実行される前にFactoryBotで生成したインスタンスを@userに代入
+    @user = build(:user)
+  end
+
+  describe "バリデーションチェック" do
+    context "失敗パターン" do
+      it "nameがない場合に、バリデーションが機能する" do
+        @user.name = ""
+        @user.valid?
+      end
+
+      it "nameは255文字以下" do
+        @user.name = "a" * 256
+        @user.valid?
+      end
+
+      it "emailがない場合に、バリデーションが機能する" do
+        @user.email = ""
+        @user.valid?
+      end
+
+      it "@マークがemailの登録にない場合に、バリデーションが機能する" do
+        @user.email.slice!("@")
+        @user.valid?
+      end
+
+      it "同じemailは登録ができない" do
+        @user.save
+        another_user = build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+      end
+
+      it "パスワードがない場合にバリデーションが機能する" do
+        @user.password = ""
+        @user.valid?
+      end
+
+      it "パスワードは６文字以上でないといけない" do
+        @user.password = "a" * 5
+        @user.valid?
+      end
+
+      it "確認用のパスワードがない場合に、バリデーションが機能する" do
+        @user.password_confirmation = ""
+        @user.valid?
+      end
+
+      it "確認用のパスワードは、入力されたパスワードと同じでないといけない。" do
+        @user.password = "password"
+        @user.password_confirmation = "12345678"
+        @user.valid?
+      end
+    end
+
+    context "成功パターン" do
+      it "条件が満たされてバリデーションに通過できる" do
+        expect(@user).to be_valid
+      end
+    end
+  end
 end
