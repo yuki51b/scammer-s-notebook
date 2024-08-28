3f3e359f05aea2ac8e1642d933baa4cf8647c297
yukimura51b <yukimura.3720@gmail.com>
2024-08-29 00:35:02 +0900

    sidekiqのリトライ回数を設定

--- app/jobs/send_line_message_job.rb
+++ app/jobs/send_line_message_job.rb
@@ -1,6 +1,15 @@
 class SendLineMessageJob < ApplicationJob
   queue_as :default
 
+  sidekiq_options retry: 1
+  # Sidekiqはジョブの実行に失敗した場合、デフォルトで25回リトライするため、
+  # 何度も通知が送られないよう、リトライ回数を1回に指定
+
+  sidekiq_retry_in do |_count|
+    60
+  end
+  # ジョブのリトライ間隔を60秒に設定
+
   def perform
     NotificationService.call
   end
