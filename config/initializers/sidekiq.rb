f2cfb0fa3fc0c70a32528b5c3ae1d9d2a4417f8f
yukimura51b <yukimura.3720@gmail.com>
2024-11-08 22:21:05 +0900

    not_credential

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: Rails.env.production? ? ENV.fetch('REDIS_URL_PRODUCTION', nil) : Rails.application.credentials.redis[:development] }
+  config.redis = { url: Rails.env.production? ? ENV.fetch('REDIS_URL_PRODUCTION', nil) : ENV.fetch('REDIS_URL_DEVELOPMENT') }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: Rails.env.production? ? ENV.fetch('REDIS_URL_PRODUCTION', nil) : Rails.application.credentials.redis[:development] }
+  config.redis = { url: Rails.env.production? ? ENV.fetch('REDIS_URL_PRODUCTION', nil) : ENV.fetch('REDIS_URL_DEVELOPMENT') }
 end
